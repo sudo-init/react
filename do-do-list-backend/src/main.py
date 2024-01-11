ccd7009caf2f6a3d47af3b6f5a535e36c878e08a
sudo-init <dep.em.k.0905@gmail.com>
2024-01-11 09:23:46 +0900

    feat: fast-api backend
    프로젝트 코드 추가

---
+++ do-do-list-backend/src/main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, Body, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get("/")
+def health_check_handler():
+    return {"ping": "pong"}
+
+
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "운동",
+        "is_done": True
+    },
+    2: {
+        "id": 2,
+        "contents": "독서",
+        "is_done": False
+    },
+    3: {
+        "id": 3,
+        "contents": "음악감상",
+        "is_done": False
+    },
+}
+
+
+@app.get("/todos", status_code=200)
+def get_todos_handler(order: str | None = None):
+    ret = list(todo_data.values())
+    if order and order == "DESC":
+        # return ret.sort(reverse=True)
+        return ret[::-1]
+    return ret
+
+
+@app.get("/todo/{todo_id}", status_code=200)
+def get_todo_handler(todo_id: int):
+    todo = todo_data.get(todo_id)
+    if todo:
+        return todo
+    return HTTPException(status_code=404, detail="ToDo Not Found")
+
+
+class CreateToDoRequest(BaseModel):
+    id: int
+    contents: str
+    is_done: bool
+
+
+@app.post("/todos", status_code=201)
+def create_todo_handler(request: CreateToDoRequest):
+    todo_data[request.id] = request.model_dump()
+    return todo_data[request.id]
+
+
+@app.patch("/todos/{todo_id}", status_code=200)
+def update_todo_handler(
+    todo_id: int,
+    is_done: bool = Body(..., embed=True),
+):
+    todo = todo_data.get(todo_id)
+    if todo:
+        todo["is_done"] = is_done
+        return todo
+    return HTTPException(status_code=404, detail="ToDo Not Found")
+
+
+@app.delete("/todos/{todo_id}", status_code=204)
+def delete_todo_handler(todo_id: int):
+    todo = todo_data.pop(todo_id, None)
+    if todo:
+        return
+    return HTTPException(status_code=404, detail="ToDo Not Found")
+
+
